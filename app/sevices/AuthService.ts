0e1a62816bb7a7e7c38b765e4e287734ddca5007
Nguyễn Quang Hoà <tieunaldo@MacbookPro-2.local>
2026-03-24 17:49:22 +0700

    fix conflix

--- app/sevices/AuthService.ts
+++ app/sevices/AuthService.ts
@@ -1,10 +1,9 @@
+import axios from "axios";
 import axiosApi from "./axiosApi";
 
 export const AuthService = {
   login: async (payload: any) => {
-    return await axiosApi
-      .post("api/login", payload)
-      .then((res) => res.data);
+    return await axiosApi.post("api/login", payload).then((res) => res.data);
   },
   forgotPassword: async (payload: any) => {
     return await axiosApi
@@ -21,5 +20,17 @@ export const AuthService = {
     return axiosApi
       .post("api/admin/Staff/RestPassword", payload)
       .then((res) => res.data);
+  },
+  async checkVersion(): Promise<null> {
+    let response = await axios.post(
+      `https://api-bms.beesky.vn/api/app/get-version`,
+      { MaLoaiApp: 1 },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return response.data;
   },
 };
